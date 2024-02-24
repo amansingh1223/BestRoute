239631b1035fbfee14a885e02f97cfc009294568
amansingh1223 <161153881+amansingh1223@users.noreply.github.com>
2024-02-24 23:35:31 +0530

    Update Order.java

--- Order.java
+++ Order.java
@@ -1,12 +1,17 @@
-// Order.java
 public class Order {
     private Restaurant restaurant;
-    
-    public Order(Restaurant restaurant) {
+    private Consumer consumer;
+
+    public Order(Restaurant restaurant, Consumer consumer) {
         this.restaurant = restaurant;
+        this.consumer = consumer;
     }
-    
+
     public Restaurant getRestaurant() {
         return restaurant;
+    }
+
+    public Consumer getConsumer() {
+        return consumer;
     }
 }
