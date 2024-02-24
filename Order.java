ce05adba74e5d7888a435f17a0e596a2f0f23887
amansingh1223 <161153881+amansingh1223@users.noreply.github.com>
2024-02-24 23:29:17 +0530

    Create Order.java

---
+++ Order.java
@@ -0,0 +1,12 @@
+// Order.java
+public class Order {
+    private Restaurant restaurant;
+    
+    public Order(Restaurant restaurant) {
+        this.restaurant = restaurant;
+    }
+    
+    public Restaurant getRestaurant() {
+        return restaurant;
+    }
+}
